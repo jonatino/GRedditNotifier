c56d3ed2579ee1fe91c22637e643c6fe931ee456
Jonatino <jonathanbeaudoin1996@gmail.com>
2018-02-12 20:25:06 -0500

    Updated gitignore, started cleaning up main code

--- GRNotifier.go
+++ GRNotifier.go
@@ -17,10 +17,18 @@ type SubReddit struct {
 }
 
 type Config struct {
-	BaseURL    string         `json:"baseurl"`
+	BaseURL    string      `json:"baseurl"`
 	Interval   int         `json:"interval"`
 	Username   string      `json:"username"`
 	SubReddits []SubReddit `json:"subreddits"`
+}
+
+type Notification struct {
+	Title     string
+	Time      string
+	URL       string
+	Author    string
+	Subreddit string
 }
 
 type UserAgentTransport struct {
@@ -44,39 +52,42 @@ func LoadConfig() Config {
 	return config
 }
 
-func parseRSSFeed(config Config, start int64, feed *gofeed.Feed) {
+func parseRSSFeed(config Config, start int64, feed *gofeed.Feed) bool {
+	//TODO possible to pass start by pointer so i don't have to return value?
 	reset := false
+
 	for _, item := range feed.Items {
 		t, _ := time.Parse(time.RFC3339, item.Updated)
-		if start < t.Unix() {
-			title := item.Title
-			link := item.Link
-			author := item.Author.Name
-			subreddit := item.Categories[0]
+		item.Updated = t.String()
 
-			fmt.Println(title + " - " + t.String() + " \n" + link + " \n" + config.BaseURL + author + " \n" + config.BaseURL + "/r/" + subreddit + "\n")
+		if start < t.Unix() {
+			notification := Notification{
+				Title:     item.Title,
+				Time:      item.Updated,
+				URL:       item.Link,
+				Author:    config.BaseURL + item.Author.Name,
+				Subreddit: config.BaseURL + "/r/" + item.Categories[0],
+			}
+
+			sendNotification(notification)
 
 			reset = true
 		}
 	}
 
-	if reset {
-		start = time.Now().Unix()
-	}
+	return reset
 }
 
 func main() {
 	config := LoadConfig()
-
-	start := time.Now().Unix()
+	start := time.Now().Unix() - 200000
 
 	fp := gofeed.NewParser()
 	fp.Client = &http.Client{
 		Transport: &UserAgentTransport{http.DefaultTransport},
 	}
 
-	fmt.Println(config.SubReddits)
-
+	reset := false
 
 	for _, subreddit := range config.SubReddits {
 		feed, e := fp.ParseURL(config.BaseURL + "/r/" + subreddit.URL + "/new/.rss")
@@ -85,6 +96,15 @@ func main() {
 			fmt.Println(e)
 		}
 
-		parseRSSFeed(config, start, feed)
+		reset = parseRSSFeed(config, start, feed)
 	}
+
+	if reset {
+		start = time.Now().Unix()
+	}
+
+}
+
+func sendNotification(n Notification) {
+	fmt.Println(n.Title + " - " + n.Time + " \n" + n.URL + " \n" + n.Author + " \n" + n.Subreddit + "\n")
 }
